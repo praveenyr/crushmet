da634cbeadd67a23d7ca0d0f35c99052563755dd
Praveen Yallakara <praveenyr@gmail.com>
2017-07-14 18:16:32 -0700

    Added rack-cors and tested CORS functionaility

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,16 @@ module Crushmet
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     Rails.application.config.generators do |g|
       g.test_framework = :rspec
     end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
